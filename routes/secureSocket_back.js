1e3d16694aa8c4faba80d890e5bd10a7d52cfee9
ColinJung <master@entercube.net>
2021-03-25 23:33:07 +0900

    v0.0988

--- routes/secureSocket_back.js
+++ routes/secureSocket_back.js
@@ -3,6 +3,9 @@
 var express 	= require('express');
 var router 	  = express.Router();
  
+const app  = express();
+
+
 const fs = require('fs');
 const https = require('https');
 /* GET users listing. 
@@ -13,26 +16,70 @@ router.get('/', function(req, res, next) {
 */
 
 
+// post 파서 
+var bodyParser = require('body-parser');            // POST 인자 파서 
+app.use(bodyParser.json());                         // POST 인자 파서 사용 
+app.use(bodyParser.urlencoded({ extended: true })); // POST 인자 인코딩 
+
+  
+// 정적 데이터 디렉토리 설정 
+app.use(express.static('public'));
+
+// 노드 라이브러리 바로 사용 v0.313 
+app.use(express.static('node_modules'));
+ 
+ 
+
+// 인증서 적용 
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/utest.soymlops.com/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/utest.soymlops.com/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/utest.soymlops.com/chain.pem', 'utf8');
+
+ 
+app.use((req, res) => {
+
+  let msg; 
+
+  msg = "Node SSL Utest-Server V1.885 is running "; 
+ 
+  console.log(msg);   // 콘솔 
+});
 // F30 ================  보안 웹소켓  ========================= 
  // 호출주소 
  // 일반접속  :  ws://serverip:88
  // 보안접속  :  wss://serverip:88  (여기에 해당)
- // wss://app.joeunname.co.kr:443/secureSocket 
+  // wss://app.joeunname.co.kr:443/secureSocket 
   
- 
+ const privateKey = fs.readFileSync('/etc/letsencrypt/live/app.joeunname.co.kr/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/app.joeunname.co.kr/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/app.joeunname.co.kr//chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(443, () => {
+    console.log('UTEST wrtc 0.282 HTTPS Server running on port 443');
+  });
+  
+
 
 const WebSocket = require('ws'); 
 
 var allmcnt     = 0;     // 전체 메시지 수량 
 var conncnt     = 0;     // 소켙 접속 횟수 (전체)
-var socketPort  = 443;  // 소켙 주소 443번으로 설정
-/*
-const wss = new WebSocket.Server({
+var socketPort  = 443;  // 소켙 주소 1030번으로 설정
+/*const wss = new WebSocket.Server({
   port: socketPort,
 });
-*/ 
+*/
 
-// httpsServer : 글로벌 선언됨 
+
 const wss = new WebSocket.Server({
   server: httpsServer
 });
