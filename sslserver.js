1ebc38536500169e55c4c2d062fe94c74b91d9dc
GaussJung <cwjung123@gmail.com>
2021-07-06 08:33:37 +0900

    v2.07

--- sslserver.js
+++ sslserver.js
@@ -81,175 +81,6 @@ const credentials = {
 };
 
 
- 
-app.use((req, res) => {
-  let msg; 
-
-  msg = "Node myApp-Server V0.918 is running "; 
- 
-  console.log(msg);   // 콘솔 
-});
-
-//  리스터 Starting both http & https servers
-// const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-/*
-httpServer.listen(80, () => {
-	console.log('myApp - A NODE HTTP Server running on port 80');
-});
-*/ 
-
-httpsServer.listen(443, () => {
-	console.log('myApp  - B NODE HTTPS Server running on port 443');
-});
-
-
-// 이하 웹소켄 접속 
- 
-var allmcnt     = 0;     // 전체 메시지 수량 
-var conncnt     = 0;     // 소켙 접속 횟수 (전체)
- 
- // 웹소켙 
- const WebSocket = require('ws'); 
-
- //  console.log(" ============== myApp Test WebServer with webSocket V0.921 ============= "); 
-   
-  // ============== 31-a secure websocket ================= 	port: 443 
-  const wss = new WebSocket.Server({
-	server: httpsServer,
-	path: "/socket"
-  });
- 
-  
-  // F31-a. socket connection test 
-  wss.on('connection', (wskt) => {
-		
-	let pfnow     = 0.0;        // 현재 시간 millisec 
-	let curmcnt   = 0.0;        // 현재메시지 수량 
-	
-	conncnt++;  // 현재 접속 수량증대 
-  
-	wskt.send(' Connected To Socket SecureWebSocket V1.7 conncnt=' + conncnt);
-  
-	// F33-1. binding message 
-	wskt.on('message', (indata) => {
-  
-	  let fmessage  = "";
-  
-	  // 현재시간 ( millisec )
-	  pfnow = process.hrtime(); 
-	  curmcnt++;  // 현재메시지 수량 
-	  allmcnt++;  // 전체 메시지 접속수량 증대 
- 
-	  // SF05. Parse Message 
-	  try {
-		// fmessage = JSON.parse(indata);
-		fmessage = indata; 
-		// console.log( "SC91 success fmessage=" + indata ); 
-	  } 
-	  catch (err) {
-		sendError(wskt, 'Wrong format Err SE-150 err=' + err);
-		return;
-	  }
-	  // EOF SF05. 
-	  let metaStr = "V1.21 wss Time=" + pfnow + " / connAll=" + conncnt + " / msgAll=" + allmcnt + " / msgCur=" + curmcnt;
-	  let finalMsg = metaStr + "\n" + fmessage;  // 최종메시지 : 메타정보 + 전달메시지 
-	 
-	  console.log( "SC92 finalMsg=" + finalMsg ); 
-  
-	  wskt.send(finalMsg); 
-	  
-	});
-	// EOF F33-1. message binding 
-  
-  });
-  // EOF F31-a  secureSocket 
-
-
- // =========== 31-b.  normal websocket ===============
- // 포트를 지정하지 않으면 기본 80 
- /*
- const webSkt = new WebSocket.Server({
-	server: httpServer,
-	port: 1000,
-	path: "/socket"
-});
-*/ 
- 
-
-//   socket connection test 
-/* 
-webSkt.on('connection', (wskt, request) => {
-      
-    // console.log(`C09. Conn Url ${request.url}`);
-    let conuri =  request.url; 
-
-    console.log( "SC10 conuri=" + conuri  ); 
-
-    let pfnow     = 0.0;        // 현재 시간 millisec 
-    let curmcnt   = 0.0;        // 현재메시지 수량 
-  
-    conncnt++;  // 현재 접속 수량증대 
-
-    wskt.send('C10 Connected To Rocket WebSocket V1.4 conncnt=' + conncnt);
-
-    // F33-1. binding message 
-    wskt.on('message', (indata) => {
-
-      let fmessage  = "";
-
-      // 현재시간 ( millisec )
-      pfnow = process.hrtime(); 
-      curmcnt++;  // 현재메시지 수량 
-      allmcnt++;  // 전체 메시지 접속수량 증대 
-    
-      // console.log( "SC90 indata=" + JSON.stringify(indata) ); 
-  
-      // SF05. Parse Message 
-      try {
-        // fmessage = JSON.parse(indata);
-        fmessage = indata; 
-        // console.log( "SC91 success fmessage=" + indata ); 
-      } 
-      catch (err) {
-        sendError(wskt, 'Wrong format Err SE-150 err=' + err);
-        return;
-      }
-      // EOF SF05. 
-      let metaStr = "V1.41 Time=" + pfnow + " / connAll=" + conncnt + " / msgAll=" + allmcnt + " / msgCur=" + curmcnt;
-      let finalMsg = metaStr + "\n" + fmessage;  // 최종메시지 : 메타정보 + 전달메시지 
-    
-      console.log( "SC92 finalMsg=" + finalMsg ); 
-
-      wskt.send(finalMsg); 
-      
-    });
-    // EOF F33-1. message binding 
- 
-});
-// EOF F31-b normal socket 
-
-*/ 
-
-// F30-c. socket Error  
-const sendError = (wskt, errmessage) => {
-
-	const messageObject = {
-	   type: 'ERROR',
-	   payload: errmessage,
-	};
-  
-	let outMsg = JSON.stringify(messageObject); 
-  
-	console.log("SC100 Error outMsg=" + outMsg); 
-  
-	// Send Error Msg 
-	wskt.send(JSON.stringify(messageObject));
-};
-  // EOF F30-c. 
-
-
   
 // ====================== SWAGGER ================================ 
 // ========== 확인 : http://localhost/api-docs/ 
@@ -276,18 +107,21 @@ app.listen(443, () => {
 });
 */ 
 
+
+ 
+app.use((req, res) => {
+  let msg; 
+  msg = "Node myApp-Server V0.92 is running "; 
+  console.log(msg);   // 콘솔 
+});
+
 // 초기접속화면 : https://domain:443/api-docs 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+ 
 
 
 console.log("============ START SWAGGER FOR SSL V1.1 ============= "); 
-
  
-
-app.listen(5000, () => {
-  console.log("\n\n\n =============== ndwrtc v0.5 5000 server listening on port 5000!");
-});
-
  
   
 // GET Call for all users
@@ -412,4 +246,94 @@ app.delete("/user/:userId", (req, res) => {
               message: 'error in delete'   
     });
 })
+
+ 
+
+
+//  리스터 Starting both http & https servers
+// const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+ 
+
+httpsServer.listen(443, () => {
+	console.log('myApp  - B NODE HTTPS Server running on port 443');
+});
+
+
+// 이하 웹소켄 접속 
+var allmcnt     = 0;     // 전체 메시지 수량 
+var conncnt     = 0;     // 소켙 접속 횟수 (전체)
+ 
+ // 웹소켙 
+ const WebSocket = require('ws'); 
+
+ //  console.log(" ============== myApp Test WebServer with webSocket V0.921 ============= "); 
+  
+// ============== 31-a secure websocket ================= 	port: 443 
+const wss = new WebSocket.Server({
+    server: httpsServer,
+    path: "/socket"
+});
+
+
+// F31-a. socket connection test 
+wss.on('connection', (wskt) => {
+  
+  let pfnow     = 0.0;        // 현재 시간 millisec 
+  let curmcnt   = 0.0;        // 현재메시지 수량 
+
+  conncnt++;  // 현재 접속 수량증대 
+
+  wskt.send(' Connected To Socket SecureWebSocket V1.7 conncnt=' + conncnt);
+
+  // F33-1. binding message 
+  wskt.on('message', (indata) => {
+
+    let fmessage  = "";
+
+    // 현재시간 ( millisec )
+    pfnow = process.hrtime(); 
+    curmcnt++;  // 현재메시지 수량 
+    allmcnt++;  // 전체 메시지 접속수량 증대 
+
+    // SF05. Parse Message 
+    try {
+    // fmessage = JSON.parse(indata);
+    fmessage = indata; 
+    // console.log( "SC91 success fmessage=" + indata ); 
+    } 
+    catch (err) {
+      sendError(wskt, 'Wrong format Err SE-150 err=' + err);
+      return;
+    }
+    // EOF SF05. 
+    let metaStr = "V1.21 wss Time=" + pfnow + " / connAll=" + conncnt + " / msgAll=" + allmcnt + " / msgCur=" + curmcnt;
+    let finalMsg = metaStr + "\n" + fmessage;  // 최종메시지 : 메타정보 + 전달메시지 
+  
+    console.log( "SC92 finalMsg=" + finalMsg ); 
+
+    wskt.send(finalMsg); 
+  
+  });
+  // EOF F33-1. message binding 
+
+});
+// EOF F31-a  secureSocket 
+
+// F30-c. socket Error  
+const sendError = (wskt, errmessage) => {
+
+	const messageObject = {
+	   type: 'ERROR',
+	   payload: errmessage,
+	};
+  
+	let outMsg = JSON.stringify(messageObject); 
+  
+	console.log("SC100 Error outMsg=" + outMsg); 
+  
+	// Send Error Msg 
+	wskt.send(JSON.stringify(messageObject));
+};
+  // EOF F30-c. 
 
