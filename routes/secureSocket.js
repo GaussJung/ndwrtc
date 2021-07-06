c43d82260d975630bb800a678d1419607c57f23d
ColinJung <master@entercube.net>
2021-07-06 23:19:41 +0900

    v2.36

--- routes/secureSocket.js
+++ routes/secureSocket.js
@@ -6,6 +6,9 @@ var express = require('express');
 var router 	= express.Router();
 const WebSocket = require('ws'); 
 
+var wss = webSkt;
+ 
+
 // F30 ================  웹소켓  ========================= 
  // 호출주소 
  // 일반접속  :  ws://serverip:1010/socket
@@ -16,7 +19,7 @@ var conncnt   = 0;     // 소켙 접속 횟수 (전체)
 //var socketPort = 1001; // 소켙 주소 1000로 설정 
 
 // F92. socket connection 설정   webSkt : 글로벌로 설정 
-webSkt.on('connection', (wskt) => {
+wss.on('connection', (wskt) => {
   
   let pfnow     = 0.0;        // 현재 시간 millisec 
   let curmcnt   = 0.0;        // 현재메시지 수량 
