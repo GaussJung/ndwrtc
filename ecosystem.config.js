76d472713856443bc0b07729387388b8fca1a83e
GaussJung <cwjung123@gmail.com>
2021-07-07 13:03:01 +0900

    v2.591

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,6 @@
         name: 'ndwrtc',
         script: './server-startup.js',
         instances: 0,
-        exec_mode: 'cluster',
-        merge_logs: true,
-        env: {
-            COMMON_VARIABLE: 'true'
-          },
-        env_production : {
-            NODE_ENV: 'production'
-        }
+        exec_mode: 'fork' 
     }]
 }
